f2e6a29376380ba88dc6864f644aef31e8a7a05e
Palash Shah <35114859+Palashio@users.noreply.github.com>
2020-10-02 17:19:36 -0400

    Create encoder.py

---
+++ encoder.py
@@ -0,0 +1,4 @@
+class Encoder:
+    def __init__(self, path):
+        '''initializer for the encoder class, reads in the data and gathers all information in one location'''
+        self.dataset_path = path
